a06bd78869a62ec581eb5526ad45e9cab6748f7e
Mit0213 <mitsurukudo213@gmail.com>
2024-08-20 03:04:33 -0500

    configured hardhat with alchemy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,35 @@
-require("@nomicfoundation/hardhat-toolbox");
+// require("@nomicfoundation/hardhat-waffle");
+// requrei('dotenv').config();
+
+// const { API_URL, PRIVATE_KEY } = process.env;
+
+// /** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: "0.8.24",
+//   networks: {
+//     rinkeby: {
+//       url: API_URL,
+//       accounts: [PRIVATE_KEY]
+//     }
+//   }
+// };
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+require('dotenv').config();
+require('@nomiclabs/hardhat-ethers');
+
+const { API_URL, PRIVATE_KEY } = process.env;
+
 module.exports = {
   solidity: "0.8.24",
+  defaultNetwork: 'sepolia',
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  }
 };
