239137774266a01d45548d1290d941d0ed23d1fc
Mit0213 <mitsurukudo213@gmail.com>
2024-08-21 04:29:07 -0500

    Modified the home page

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -28,12 +28,12 @@ function Home() {
       console.log('No Wallet found. Please connect Wallet')
     }
 
-    const accounts = await ethereum.request({ method: 'eth_accounts' })
-    if (accounts.length !== 0) {
-      console.log('Found authorized Account: ', accounts[0])
-    } else {
-      console.log('No authorized account found, please add account to your wallet')
-    }
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts' })
+      if (accounts.length !== 0) {
+        console.log('Found authorized Account: ', accounts[0])
+      } else {
+        console.log('No authorized account found, please add account to your wallet')
+      }
   }
 
   //Calls metamask to connect wallet on clicking Connect Wallet Button
